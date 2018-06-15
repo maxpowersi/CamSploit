1286ae92f9dc88af9034d2d43932ded5aa4211e6
Maxpowersi <maxi1990@gmail.com>
2018-06-15 15:34:02 -0300

    New architecture created

--- CamSploit/Program.cs
+++ CamSploit/Program.cs
@@ -1,8 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using CamSploit.CVEs;
 using CommandLine;
+using ExploitMaker;
 
 namespace CamSploit
 {
--- CamSploit/Writter.cs
+++ CamSploit/Writter.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using ExploitMaker;
 
 namespace CamSploit
 {
