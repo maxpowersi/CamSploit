c46f95e5a3ef3053109e5016328166e4a695dbac
Maxpowersi <maxi1990@gmail.com>
2018-06-19 14:16:49 -0300

    Bug Fixing

--- CamSploit/Writter.cs
+++ CamSploit/Writter.cs
@@ -7,16 +7,16 @@ namespace CamSploit
 {
     public class Writter : IWritter
     {
-        private TxtFile _txtFile;
+        private readonly StreamWriter _txtFile;
 
         public Writter(string outputPath)
         {
             if (!Path.IsPathRooted(outputPath))
                 outputPath = Path.Combine(Path.GetDirectoryName(Assembly.GetEntryAssembly().Location), outputPath);
             
-            Init(outputPath);
+            _txtFile = new StreamWriter(outputPath);
         }
-
+        
         public void Dispose()
         {
             _txtFile.Dispose();
@@ -26,30 +26,28 @@ namespace CamSploit
         {
             Console.WriteLine(Phrases.Init_Test, cve, cam.Address);
         }
+        
+        public void TestSuccess(string cve, Camera cam, Credencial cred)
+        {
+            Console.WriteLine(Phrases.Test_Success, cam.Address, cve, cred);
+
+            _txtFile.Write(cam + "," + cred.Username + "," + cred.Password + "," + cve + ", Success," + "");
+            _txtFile.Flush();
+        }
 
         public void TestFailed(string cve, Camera cam)
         {
             Console.WriteLine(Phrases.Test_File, cam.Address, cve);
 
-            _txtFile?.Write(cam + ",,," + cve);
+            _txtFile.Write(cam + ",null,null," + cve + ", Fail," + "");
+            _txtFile.Flush();
         }
 
-        public void TestSuccess(string cve, Camera cam, Credencial cred)
-        {
-            Console.WriteLine(Phrases.Test_Success, cam.Address, cve, cred);
-
-            _txtFile?.Write(cam + "," + cred.Username + "," + cred.Password + "," + cve);
-        }
-
-        public void LogError(string cve, Camera cam, string error)
+        public void TestFailedMsg(string cve, Camera cam, string error)
         {
             Console.WriteLine(error, cam.Address, cve);
-        }
-
-        private void Init(string output)
-        {
-            _txtFile = new TxtFile(output);
-            _txtFile.Create();
+            _txtFile.Write(cam + ",null,null," + cve + ", Fail," + string.Format(error, cam.Address, cve));
+            _txtFile.Flush();
         }
     }
 }
